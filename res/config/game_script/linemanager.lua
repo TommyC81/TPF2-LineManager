6dec4ffac8c77ebdfa96f5d75fcb5313ded358f9
Tommy Carlsson <tommyc81@gmail.com>
2021-08-22 16:28:57 +0400

    Update linemanager.lua

--- res/config/game_script/linemanager.lua
+++ res/config/game_script/linemanager.lua
@@ -118,11 +118,11 @@ local function updatePassengerLines()
 			totalVehicleCount = totalVehicleCount + sampledLineData[line_id].vehicles			
 			
 			if sampledLineData[line_id].samples and sampledLineData[line_id].samples >= sample_size then
-				if sampledLineData[line_id].demand > 2 * sampledLineData[line_id].rate or (sampledLineData[line_id].usage > 95 and sampledLineData[line_id].demand > sampledLineData[line_id].rate) or (sampledLineData[line_id].usage > 80 and sampledLineData[line_id].demand > ((sampledLineData[line_id].vehicles + 1) / sampledLineData[line_id].vehicles) * sampledLineData[line_id].rate) then
+				if sampledLineData[line_id].demand > 2 * sampledLineData[line_id].rate or (sampledLineData[line_id].usage > 95 and sampledLineData[line_id].demand > sampledLineData[line_id].rate) or (sampledLineData[line_id].usage > 80 and sampledLineData[line_id].demand > sampledLineData[line_id].rate * (sampledLineData[line_id].vehicles + 1) / sampledLineData[line_id].vehicles) then
 					print("Line: " .. helper.getLineName(line_id) .. " (" .. line_id .. ") - Usage: " .. sampledLineData[line_id].usage .. "% (" .. sampledLineData[line_id].occupancy .. "/" .. sampledLineData[line_id].capacity .. ") Veh: " .. sampledLineData[line_id].vehicles .. " Demand: " .. sampledLineData[line_id].demand .. " Rate: " .. sampledLineData[line_id].rate)
 					addVehicle(line_id)
 					sampledLineData[line_id].samples = sample_size - 2 * sample_interval
-				elseif sampledLineData[line_id].vehicles > 1 and sampledLineData[line_id].usage < 50 and sampledLineData[line_id].demand < sampledLineData[line_id].rate then
+				elseif sampledLineData[line_id].vehicles > 1 and ((sampledLineData[line_id].usage < 60 and sampledLineData[line_id].demand < sampledLineData[line_id].rate) or (sampledLineData[line_id].usage < 80 and sampledLineData[line_id].demand < sampledLineData[line_id].rate * (sampledLineData[line_id].vehicles - 1) / sampledLineData[line_id].vehicles)) then
 					print("Line: " .. helper.getLineName(line_id) .. " (" .. line_id .. ") - Usage: " .. sampledLineData[line_id].usage .. "% (" .. sampledLineData[line_id].occupancy .. "/" .. sampledLineData[line_id].capacity .. ") Veh: " .. sampledLineData[line_id].vehicles .. " Demand: " .. sampledLineData[line_id].demand .. " Rate: " .. sampledLineData[line_id].rate)
 					removeVehicle(line_id)
 					sampledLineData[line_id].samples = sample_size - 2 * sample_interval
