1f44466a1e548ec858eb699dad57024257135466
Tommy Carlsson <tommyc81@gmail.com>
2021-08-24 16:54:57 +0400

    Version number update.
    Version update for good measure.

--- mod.lua
+++ mod.lua
@@ -3,7 +3,7 @@ function data()
 		info = {
 			name = _("Name"),
 			description = _("Description"),
-			minorVersion = 2,
+			minorVersion = 3,
 			severityAdd = "NONE",
 			severityRemove = "NONE",
 			tags = { "Script Mod" },
