da7baf40d7a9dc7bff5217c17adbda2805444db4
iosterwal <iosterwal@ethz.ch>
2021-12-23 01:10:22 +0100

    Now that's hopefully a good revert
    Signed-off-by: iosterwal <iosterwal@ethz.ch>

--- res/config/game_script/linemanager.lua
+++ res/config/game_script/linemanager.lua
@@ -8,11 +8,11 @@ local update_interval = 2 -- For every x sampling, do a vehicle update (check if
 local sample_restart = 2 -- Following an update of a Line, the number of recorded samples will be reset to this value for the line to delay an update until sufficient data is available
 local samples_since_last_update = 0
 
-local function removeVehicle(line_id)	
+local function removeVehicle(line_id)
 	local lineVehicles = api.engine.system.transportVehicleSystem.getLineVehicles(line_id)
 	local oldestVehicleId = 0
 	local oldestVehiclePurchaseTime = 999999999999
-	
+
 	-- Find the oldest vehicle on the line
 	for _, vehicle_id in pairs(lineVehicles) do
 		local vehicleInfo = api.engine.getComponent(vehicle_id, api.type.ComponentType.TRANSPORT_VEHICLE)
@@ -21,19 +21,19 @@ local function removeVehicle(line_id)
 			oldestVehicleId = vehicle_id
 		end
 	end
-	
+
 	-- Remove/sell the oldest vehicle (instantly sells)
 	api.cmd.sendCommand(api.cmd.make.sellVehicle(oldestVehicleId))
 	print("      Removed vehicle: " .. oldestVehicleId .. " from line: " .. line_id)
 end
-	
+
 local function addVehicle(line_id)
 	local lineVehicles = api.engine.system.transportVehicleSystem.getLineVehicles(line_id)
 	local depot_id = nil
 	local stop_id = nil
 	local vehicleToDuplicate = nil
 	local purchaseTime = helper.getGameTime()
-	
+
 	-- TODO: Figure out a better way to find the closest depot (or one at all).
 	-- This merely tries to send an existing vehicle on the line to the depot, checks if succeeds then cancel the depot call but uses the depot data.
 	-- Unfortunately sending a vehicle to a depot empties the vechicle.
@@ -43,10 +43,11 @@ local function addVehicle(line_id)
 		if helper.vehicleTransportsPassengers(vehicle_id) then
 			api.cmd.sendCommand(api.cmd.make.sendToDepot(vehicle_id, false))
 			vehicleToDuplicate = api.engine.getComponent(vehicle_id, api.type.ComponentType.TRANSPORT_VEHICLE)
-		
+
 			if vehicleToDuplicate.state == api.type.enum.TransportVehicleState.GOING_TO_DEPOT then
 				depot_id = vehicleToDuplicate.depot
-				api.cmd.sendCommand(api.cmd.make.setLine(vehicle_id, line_id, vehicleToDuplicate.stopIndex))
+				stop_id = vehicleToDuplicate.stopIndex
+				api.cmd.sendCommand(api.cmd.make.setLine(vehicle_id, line_id, stop_id))
 				break
 			end
 		end
@@ -54,13 +55,13 @@ local function addVehicle(line_id)
 
 	if depot_id then
 		local transportVehicleConfig = vehicleToDuplicate.transportVehicleConfig
-		
+
 		-- Reset applicable parts of the transportVehicleConfig
 		for _, vehicle in pairs(transportVehicleConfig.vehicles) do
 			vehicle.purchaseTime = purchaseTime
 			vehicle.maintenanceState = 1
 		end
-		
+
 		-- TODO: This doesn't return the id of the new vehicle, instead I check that the purchaseTime corresponds to the expected.
 		-- This is not perfect, but shouldn't be a big issue. In the API documentation this should return an id.
 		-- api.type.BuyVehicle(playerEntity, depotEntity, config) return resultVehicleEntity
@@ -68,20 +69,20 @@ local function addVehicle(line_id)
 		local depot_vehicles = api.engine.system.transportVehicleSystem.getDepotVehicles(depot_id)
 		for _, depot_vehicle_id in pairs(depot_vehicles) do
 			local depot_vehicle = api.engine.getComponent(depot_vehicle_id, api.type.ComponentType.TRANSPORT_VEHICLE)
-			if depot_vehicle.transportVehicleConfig.vehicles[1].purchaseTime == purchaseTime then			
-				api.cmd.sendCommand(api.cmd.make.setLine(depot_vehicle_id, line_id, 1))
+			if depot_vehicle.transportVehicleConfig.vehicles[1].purchaseTime == purchaseTime then
+				api.cmd.sendCommand(api.cmd.make.setLine(depot_vehicle_id, line_id, stop_id))
 				print("      Added vehicle: " .. depot_vehicle_id .. " to line: " .. line_id .. " via depot: " .. depot_id)
 			end
 		end
 	else
 		print("Unable to add vehicle to line: " .. line_id .. " No available depot.")
-	end	
+	end
 end
 
 local function sampleLines()
 	log.info("============ Sampling ============")
 	local lineData = helper.getLineData()
-	
+
 	for line_id, line_data in pairs(lineData) do
 		if sampledLineData[line_id] then
 			lineData[line_id].samples = sampledLineData[line_id].samples + 1
@@ -95,33 +96,33 @@ local function sampleLines()
 			lineData[line_id].samples = 1
 		end
 	end
-	
+
 	-- By initially just using the fresh lineData, no longer existing lines are removed. Does this cause increased memory/CPU usage?
 	sampledLineData = lineData
 end
 
 local function updateLines()
 	log.info("============ Updating ============")
-	local lines = api.engine.system.lineSystem.getLines()
+	local lines = helper.getPlayerLines()
 	local lineCount = 0
 	local totalVehicleCount = 0
-	
+
 	for _, line_id in pairs(lines) do
 		-- TODO: Should check that the line still exists, and still transports passengers.
 		if sampledLineData[line_id] then
 			lineCount = lineCount + 1
-			totalVehicleCount = totalVehicleCount + sampledLineData[line_id].vehicles			
-			
+			totalVehicleCount = totalVehicleCount + sampledLineData[line_id].vehicles
+
 			-- If a line has sufficient samples, then check whether vehicles should be added/removed.
 			if sampledLineData[line_id].samples and sampledLineData[line_id].samples >= sample_size then
 				-- Check if a vehicle should be added to a Line.
-				if (sampledLineData[line_id].usage > 95 and sampledLineData[line_id].demand > sampledLineData[line_id].rate) or (sampledLineData[line_id].usage > 80 and sampledLineData[line_id].demand > sampledLineData[line_id].rate * 2) then
+				if (sampledLineData[line_id].usage > 50 and sampledLineData[line_id].demand > sampledLineData[line_id].rate * 2) or (sampledLineData[line_id].usage > 80 and sampledLineData[line_id].demand > sampledLineData[line_id].rate * (sampledLineData[line_id].vehicles + 1) / sampledLineData[line_id].vehicles) then
 					print("Line: " .. helper.getEntityName(line_id) .. " (" .. line_id .. ") - Usage: " .. sampledLineData[line_id].usage .. "% (" .. sampledLineData[line_id].occupancy .. "/" .. sampledLineData[line_id].capacity .. ") Veh: " .. sampledLineData[line_id].vehicles .. " Demand: " .. sampledLineData[line_id].demand .. " Rate: " .. sampledLineData[line_id].rate)
 					sampledLineData[line_id].samples = sample_restart
 					addVehicle(line_id)
 					totalVehicleCount = totalVehicleCount + 1
-				-- Check instead whether a vehicle should be removed from a Line.
-				elseif sampledLineData[line_id].vehicles > 1 and sampledLineData[line_id].usage < 90 * (sampledLineData[line_id].vehicles - 1) / sampledLineData[line_id].vehicles and sampledLineData[line_id].demand < sampledLineData[line_id].rate then
+					-- Check instead whether a vehicle should be removed from a Line.
+				elseif sampledLineData[line_id].vehicles > 1 and sampledLineData[line_id].usage < 70 and sampledLineData[line_id].demand < sampledLineData[line_id].rate * (sampledLineData[line_id].vehicles - 1) / sampledLineData[line_id].vehicles then
 					print("Line: " .. helper.getEntityName(line_id) .. " (" .. line_id .. ") - Usage: " .. sampledLineData[line_id].usage .. "% (" .. sampledLineData[line_id].occupancy .. "/" .. sampledLineData[line_id].capacity .. ") Veh: " .. sampledLineData[line_id].vehicles .. " Demand: " .. sampledLineData[line_id].demand .. " Rate: " .. sampledLineData[line_id].rate)
 					sampledLineData[line_id].samples = sample_restart
 					removeVehicle(line_id)
@@ -135,19 +136,19 @@ end
 
 local function checkIfUpdateIsDue()
 	local current_month = helper.getGameMonth()
-	
+
 	-- Check if the month has changed since last sample. If so, do another sample. 1 sample/month.
 	if last_sampled_month ~= current_month then
 		last_sampled_month = current_month
-		
+
 		sampleLines()
 		samples_since_last_update = samples_since_last_update + 1
-		
+
 		if samples_since_last_update >= update_interval then
-			updateLines()			
+			updateLines()
 			samples_since_last_update = 0
 		end
-	end	
+	end
 end
 
 local function update()
@@ -158,4 +159,4 @@ function data()
 	return {
 		update = update
 	}
- end
+end
--- res/scripts/cartok/helper.lua
+++ res/scripts/cartok/helper.lua
@@ -22,7 +22,7 @@ function helper.vehicleTransportsPassengers(vehicle_id)
 	if type(vehicle_id) == "string" then vehicle_id = tonumber(vehicle_id) end
 	if not(type(vehicle_id) == "number") then return false end
 
-    local vehicleInfo = api.engine.getComponent(vehicle_id, api.type.ComponentType.TRANSPORT_VEHICLE)
+	local vehicleInfo = api.engine.getComponent(vehicle_id, api.type.ComponentType.TRANSPORT_VEHICLE)
 	if vehicleInfo and vehicleInfo.config and vehicleInfo.config.capacities[1] and vehicleInfo.config.capacities[1] > 0 then
 		return true
 	else
@@ -72,52 +72,57 @@ function helper.lineHasType(line_id, lineType)
 	if vehicles and vehicles[1] then
 		local component = api.engine.getComponent(vehicles[1], api.type.ComponentType.TRANSPORT_VEHICLE)
 		if component and component.carrier then
-			return component.carrier  == api.type.enum.Carrier[lineType]
+			return component.carrier == api.type.enum.Carrier[lineType]
 		end
 	end
 	return false
 end
 
+-- Returns all lines for the Player
+function helper.getPlayerLines()
+	return api.engine.system.lineSystem.getLinesForPlayer(api.engine.util.getPlayer())
+end
+
 -- returns Array, containing line_id, vehicles, capacity, occupancy, usage, demand and rate
 function helper.getLineData()
-	local lines = api.engine.system.lineSystem.getLinesForPlayer(api.engine.util.getPlayer())
+	local lines = helper.getPlayerLines()
 	local lineData = {}
 	local totalVehicleCount = 0
-	
+
 	for _, line_id in pairs(lines) do
 		-- Check type of line first
 		local line = api.engine.getComponent(line_id, api.type.ComponentType.LINE)
-		-- transportModes[4] = ROAD, transportModes[7] = TRAM
-		if line and line.vehicleInfo and line.vehicleInfo.transportModes and (line.vehicleInfo.transportModes[4] == 1 or line.vehicleInfo.transportModes[7] == 1) then
+		-- transportModes[4] = ROAD, transportModes[7] = TRAM, transportModes[10] = AIR, transportModes[13] = WATER
+		if line and line.vehicleInfo and line.vehicleInfo.transportModes and (line.vehicleInfo.transportModes[4] == 1 or line.vehicleInfo.transportModes[7] == 1 or line.vehicleInfo.transportModes[10] == 1 or line.vehicleInfo.transportModes[13] == 1) then
 			local lineVehicleCount = 0
 			local lineCapacity = 0
 			local lineOccupancy = 0
 			local lineTravellerCount = 0
-		
+
 			local lineTravellers = api.engine.system.simPersonSystem.getSimPersonsForLine(line_id)
 			for _, traveller_id in pairs(lineTravellers) do
 				lineTravellerCount = lineTravellerCount + 1
 			end
-			
+
 			if lineTravellerCount > 0 then
 				local lineVehicles = api.engine.system.transportVehicleSystem.getLineVehicles(line_id)
-				for _, vehicle_id in pairs(lineVehicles) do		
+				for _, vehicle_id in pairs(lineVehicles) do
 					local vehicle = api.engine.getComponent(vehicle_id, api.type.ComponentType.TRANSPORT_VEHICLE)
 					if vehicle and vehicle.config and vehicle.config.capacities[1] and vehicle.config.capacities[1] > 0 then
 						lineVehicleCount = lineVehicleCount + 1
 						lineCapacity = lineCapacity + vehicle.config.capacities[1]
 					end
-					
+
 					for _, traveller_id in pairs(lineTravellers) do
 						local traveller = api.engine.getComponent(traveller_id, api.type.ComponentType.SIM_PERSON_AT_VEHICLE)
-						if traveller and traveller.vehicle and traveller.vehicle == vehicle_id then	
+						if traveller and traveller.vehicle and traveller.vehicle == vehicle_id then
 							lineOccupancy = lineOccupancy + 1
 						end
 					end
-				end				
+				end
 
 				lineData[line_id] = {vehicles = lineVehicleCount, capacity = lineCapacity, occupancy = lineOccupancy, demand = lineTravellerCount, usage = math.round(100*lineOccupancy/lineCapacity), rate = helper.getLineRate(line_id)}
-			end			
+			end
 		end
 	end
 
@@ -138,9 +143,9 @@ return helper
 --      [7] = 0, TRAM
 --      [8] = 0,
 --      [9] = 0, RAIL
---      [10] = 0,
+--      [10] = 0, AIR
 --      [11] = 0,
---      [12] = 0, AIR
+--      [12] = 0,
 --      [13] = 0, WATER
 --      [14] = 0,
 --      [15] = 0,
