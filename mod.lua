02298846f1604b4074cfadee50493060bd44e740
Tommy Carlsson <tommyc81@gmail.com>
2021-12-27 09:10:35 +0400

    Prepare v1.6
    Update helper.lua
    
    Overhaul based on contribution from RusteyBucket. Expanded types and logging.
    
    Update .gitignore
    
    Add IDE stuff.
    
    Create .lua-format
    
    Initial version.
    
    Update mod.lua
    
    Updated version.
    
    Update README.md
    
    General update and attribution.
    
    Update strings.lua
    
    General update and attribution.
    
    Update logging.lua
    
    Add function to determine if debugging or not. Add setting for verbose debugging and associated functions. Make use of the default setting in setLevel.
    
    Update linemanager.lua
    
    Overhaul based on contribution from RusteyBucket. Expanded types and logging.

--- mod.lua
+++ mod.lua
@@ -3,7 +3,7 @@ function data()
 		info = {
 			name = _("Name"),
 			description = _("Description"),
-			minorVersion = 4,
+			minorVersion = 6,
 			severityAdd = "NONE",
 			severityRemove = "NONE",
 			tags = { "Script Mod" },
--- res/scripts/cartok/helper.lua
+++ res/scripts/cartok/helper.lua
@@ -1,15 +1,13 @@
 -- Contains code from https://github.com/IncredibleHannes/TPF2-Timetables
 ---@author CARTOK wrote the bulk of the code while
----@author RusteyBucket rearranged it for readability with minor logic modifications where necessary for largely debug
+---@author RusteyBucket rearranged the code to improve readability and expandability
 
 local helper = {}
 
-local sortBy = " " --End of the prefix symbol between "", leave false to disable
-
----@param data userdata : the sampledLineData
----@param id number : the internal line number
----@return boolean : whether there shall be a new vehicle added or not
-function helper.moreVehicleConditions(data, id)
+---@param data userdata : the LineData (from helper.getLineData)
+---@param id number : the id of the line
+---@return boolean : whether a vehicle should be added to the line
+function helper.moreVehicleConditions( data, id )
     -- a bunch of factors
     local usage = data[id].usage
     local demand = data[id].demand
@@ -32,10 +30,10 @@ function helper.moreVehicleConditions(data, id)
     return res
 end
 
----@param data userdata : the sampledLineData variable
----@param id number : the internal line number
----@return boolean : whether the line warrants less vehicles
-function helper.lessVehiclesConditions(data, id)
+---@param data userdata : the LineData (from helper.getLineData)
+---@param id number : the id of the line
+---@return boolean : whether a vehicle should be removed from the line
+function helper.lessVehiclesConditions( data, id )
     -- a bunch of factors
     local usage = data[id].usage
     local demand = data[id].demand
@@ -57,18 +55,19 @@ function helper.lessVehiclesConditions(data, id)
     return res
 end
 
----@param id number : general line info
----@return boolean : or error, thus unsafe
-function helper.supportedRoute(id)
+---@param line_id number : the id of the line
+---@return boolean : whether the line type is supported by the mod
+function helper.supportedLine( line_id )
     -- get the line info
-    local line = api.engine.getComponent(id, api.type.ComponentType.LINE)
+    local line = api.engine.getComponent( line_id, api.type.ComponentType.LINE )
     local info = line.vehicleInfo.transportModes
 
     -- check whether the parameters actually work
     if not line and line.vehicleInfo and line.vehicleInfo.transportModes then
         return false
     end
-    -- 4 = ROAD, 6 = TRAM, 7 = ELECTRIC_TRAM, 10 = AIR, 11 = WATER_LARGE, 13 = WATER_SMALL
+    -- 4 = ROAD, 6 = TRAM, 7 = ELECTRIC_TRAM, 10 = AIR?, 11 = WATER_LARGE, 12 = AIR, 13 = WATER_SMALL
+    -- TODO: Confirm type 10
     local modes = {
         info[4],
         info[6],
@@ -76,7 +75,7 @@ function helper.supportedRoute(id)
         info[10],
         info[11],
         info[12],
-        info[13]
+        info[13],
     }
 
     local res = false
@@ -88,30 +87,29 @@ function helper.supportedRoute(id)
     return res
 end
 
----@param data userdata
----@param id number
+---@param data userdata : the LineData (from helper.getLineData)
+---@param line_id number : the id of the line
 ---@return string
 ---returns usage data string
-function helper.lineDump(data, id)
-    local use = "Usage: " .. data[id].usage .. "% "
-    use = use .. "(" .. data[id].occupancy .. "/" .. data[id].capacity .. ") "
-    use = use .. "Veh: " .. data[id].vehicles .. " "
-    use = use .. "Demand: " .. data[id].demand .. " "
-    use = use .. "Rate: " .. data[id].rate
+function helper.printLineData( data, line_id )
+    local use =  "Usage: " .. data[line_id].usage .. "% "
+    use = use .. "Demand: " .. data[line_id].demand .. " "
+    use = use .. "Rate: " .. data[line_id].rate .. " "
+    use = use .. "Vehicles: " .. data[line_id].vehicles
     return use
 end
 
----@param line_id number | string
+---@param line_id number | string : the id of the line
 ---@return number : lineRate
-function helper.getLineRate(line_id)
-    if type(line_id) == "string" then
-        line_id = tonumber(line_id)
+function helper.getLineRate( line_id )
+    if type( line_id ) == "string" then
+        line_id = tonumber( line_id )
     end
-    if not (type(line_id) == "number") then
+    if not (type( line_id ) == "number") then
         return 0
     end
 
-    local lineEntity = game.interface.getEntity(line_id)
+    local lineEntity = game.interface.getEntity( line_id )
     if lineEntity and lineEntity.rate then
         return lineEntity.rate
     else
@@ -119,21 +117,18 @@ function helper.getLineRate(line_id)
     end
 end
 
----@param vehicle_id number | string
+---@param vehicle_id number | string : the id of the vehicle
 ---@return boolean : transportsPassengers
-function helper.vehicleTransportsPassengers(vehicle_id)
-    if type(vehicle_id) == "string" then
-        vehicle_id = tonumber(vehicle_id)
+function helper.vehicleTransportsPassengers( vehicle_id )
+    if type( vehicle_id ) == "string" then
+        vehicle_id = tonumber( vehicle_id )
     end
-    if not (type(vehicle_id) == "number") then
+    if not (type( vehicle_id ) == "number") then
         return false
     end
 
-    local vehicleInfo = api.engine.getComponent(vehicle_id, api.type.ComponentType.TRANSPORT_VEHICLE)
-    if vehicleInfo and
-            vehicleInfo.config and
-            vehicleInfo.config.capacities[1] and
-            vehicleInfo.config.capacities[1] > 0 then
+    local vehicleInfo = api.engine.getComponent( vehicle_id, api.type.ComponentType.TRANSPORT_VEHICLE )
+    if vehicleInfo and vehicleInfo.config and vehicleInfo.config.capacities[1] and vehicleInfo.config.capacities[1] > 0 then
         return true
     else
         return false
@@ -145,19 +140,19 @@ function helper.getGameMonth()
     return game.interface.getGameTime().date.month
 end
 
----@param entity_id number | string
+---@param entity_id number | string : the id of the entity
 ---@return string : entityName
-function helper.getEntityName(entity_id)
-    if type(entity_id) == "string" then
-        entity_id = tonumber(entity_id)
+function helper.getEntityName( entity_id )
+    if type( entity_id ) == "string" then
+        entity_id = tonumber( entity_id )
     end
-    if not (type(entity_id) == "number") then
+    if not (type( entity_id ) == "number") then
         return "ERROR"
     end
 
-    local err, res = pcall(function()
-        return api.engine.getComponent(entity_id, api.type.ComponentType.NAME)
-    end)
+    local err, res = pcall( function()
+        return api.engine.getComponent( entity_id, api.type.ComponentType.NAME )
+    end )
     if err and res and res.name then
         return res.name
     else
@@ -167,7 +162,7 @@ end
 
 ---@return number : current GameTime (milliseconds)
 function helper.getGameTime()
-    local time = api.engine.getComponent(0, api.type.ComponentType.GAME_TIME).gameTime
+    local time = api.engine.getComponent( 0, api.type.ComponentType.GAME_TIME ).gameTime
     if time then
         return time
     else
@@ -175,21 +170,21 @@ function helper.getGameTime()
     end
 end
 
----@param line_id  number | string
----@param lineType string, eg "RAIL", "ROAD", "TRAM", "WATER", "AIR"
----@return boolean : is lineType correct
-function helper.lineHasType(line_id, lineType)
-    if type(line_id) == "string" then
-        line_id = tonumber(line_id)
+---@param line_id  number | string : the id of the line
+---@param lineType string : eg "RAIL", "ROAD", "TRAM", "WATER", "AIR"
+---@return boolean : whether the line of the provided lineType
+function helper.lineHasType( line_id, lineType )
+    if type( line_id ) == "string" then
+        line_id = tonumber( line_id )
     end
-    if not (type(line_id) == "number") then
-        print("Expected String or Number")
+    if not (type( line_id ) == "number") then
+        print( "Expected String or Number" )
         return -1
     end
 
-    local vehicles = api.engine.system.transportVehicleSystem.getLineVehicles(line_id)
+    local vehicles = api.engine.system.transportVehicleSystem.getLineVehicles( line_id )
     if vehicles and vehicles[1] then
-        local component = api.engine.getComponent(vehicles[1], api.type.ComponentType.TRANSPORT_VEHICLE)
+        local component = api.engine.getComponent( vehicles[1], api.type.ComponentType.TRANSPORT_VEHICLE )
         if component and component.carrier then
             return component.carrier == api.type.enum.Carrier[lineType]
         end
@@ -199,41 +194,44 @@ end
 
 ---@return table : all lines for the Player
 function helper.getPlayerLines()
-    return api.engine.system.lineSystem.getLinesForPlayer(api.engine.util.getPlayer())
+    return api.engine.system.lineSystem.getLinesForPlayer( api.engine.util.getPlayer() )
 end
 
 ---@return table : containing line_id, vehicles, capacity, occupancy, usage, demand and rate
+---@return table : id of ignored lines
 function helper.getLineData()
     local lines = helper.getPlayerLines()
     local lineData = {}
-    local ignored = {}
-    local totalVehicleCount = 0
+    local ignoredLines = {}
 
-    for _, line_id in pairs(lines) do
+    for _, line_id in pairs( lines ) do
+        local ignoredLine = true
+
         -- Check type of line first
-        if helper.supportedRoute(line_id) then
+        if helper.supportedLine( line_id ) then
             local lineVehicleCount = 0
             local lineCapacity = 0
             local lineOccupancy = 0
             local lineTravellerCount = 0
 
-            local lineTravellers = api.engine.system.simPersonSystem.getSimPersonsForLine(line_id)
-            for _, traveller_id in pairs(lineTravellers) do
+            local lineTravellers = api.engine.system.simPersonSystem.getSimPersonsForLine( line_id )
+            for _, traveller_id in pairs( lineTravellers ) do
                 lineTravellerCount = lineTravellerCount + 1
             end
 
             if lineTravellerCount > 0 then
-                local lineVehicles = api.engine.system.transportVehicleSystem.getLineVehicles(line_id)
-                for _, vehicle_id in pairs(lineVehicles) do
-                    local vehicle = api.engine.getComponent(vehicle_id, api.type.ComponentType.TRANSPORT_VEHICLE)
+                ignoredLine = false -- This line is supported and has passengers, and is thus not ignored
+
+                local lineVehicles = api.engine.system.transportVehicleSystem.getLineVehicles( line_id )
+                for _, vehicle_id in pairs( lineVehicles ) do
+                    local vehicle = api.engine.getComponent( vehicle_id, api.type.ComponentType.TRANSPORT_VEHICLE )
                     if vehicle and vehicle.config and vehicle.config.capacities[1] and vehicle.config.capacities[1] > 0 then
                         lineVehicleCount = lineVehicleCount + 1
                         lineCapacity = lineCapacity + vehicle.config.capacities[1]
                     end
 
-                    for _, traveller_id in pairs(lineTravellers) do
-                        local traveller = api.engine.getComponent(traveller_id,
-                                api.type.ComponentType.SIM_PERSON_AT_VEHICLE)
+                    for _, traveller_id in pairs( lineTravellers ) do
+                        local traveller = api.engine.getComponent( traveller_id, api.type.ComponentType.SIM_PERSON_AT_VEHICLE )
                         if traveller and traveller.vehicle and traveller.vehicle == vehicle_id then
                             lineOccupancy = lineOccupancy + 1
                         end
@@ -245,83 +243,108 @@ function helper.getLineData()
                     capacity = lineCapacity,
                     occupancy = lineOccupancy,
                     demand = lineTravellerCount,
-                    usage = math.round(100 * lineOccupancy / lineCapacity),
-                    rate = helper.getLineRate(line_id),
-                    name = helper.getEntityName(line_id),
+                    usage = math.round( 100 * lineOccupancy / lineCapacity ),
+                    rate = helper.getLineRate( line_id ),
+                    name = helper.getEntityName( line_id )
                 }
-            else
-                table.insert(ignored, line_id)
             end
-        else
-            table.insert(ignored, line_id)
+        end
+
+        if ignoredLine then
+            table.insert( ignoredLines, line_id )
         end
     end
 
-    return lineData, ignored
+    return lineData, ignoredLines
 end
 
----strings together line name and line id depending on what is chosen in linemanager.lua
----@param id number : line ID
----@param num boolean : whether to print numbers
----@param nam boolean : whether to print names
+---strings together line name and line id depending on the provided settings
+---@param line_id number : the id of the line
+---@param printLineNumber boolean : (optional) whether to print the line number, default true
+---@param printLineName boolean : (optional) whether to print the line name, default true
 ---@return string : the line name in desired form
-function helper.identify(id, num, nam)
-    local name = helper.getEntityName(id)
-    local res
-    if (num and nam) then
-        res = name .. "(" .. id .. ")"
-    elseif (nam) then
-        res = name
-    elseif (num) then
-        res = id
+function helper.printLine( line_id, printLineNumber, printLineName )
+    printLineNumber = printLineNumber or true
+    printLineName = printLineName or true
+
+    local output = ""
+    local name = helper.getEntityName( line_id )
+
+    if (printLineNumber and printLineName) then
+        output = name .. " (" .. line_id .. ")"
+    elseif (printLineName) then
+        output = name
+    elseif (printLineNumber) then
+        output = line_id
     end
-    return res
+
+    return output
 end
 
----slightly specialized array to string that inserts a linebreak whenever the first word in the array changes
----@param introduction string : the leading sentence of the data
+---Converts array to string and optionally inserts a line break whenever the first word in the array changes
 ---@param array table : the array to be strung up
----@return string : introduction + array + some line breaks if applicable
-function helper.stringUp(introduction, array, nam)
-    local first = ""
+---@param prefixSplit string : (optional) the divider between prefix and individual name, default " "
+---@param insertLineBreak boolean : (optional) whether to insert line breaks or not, default true
+---@return string : array + some line breaks if applicable
+function helper.printArrayWithBreaks( array, prefixSplit, insertLineBreak )
+    prefixSplit = prefixSplit or " "
+    insertLineBreak = insertLineBreak or true
 
-    --space the lines by prefix
+    local output = ""
+    local previousPrefix = ""
+
+    -- space the lines by prefix
     for i = 1, #array do
-        local start = tostring(array[i])
-        if (sortBy and nam) then
-            local prefEnd = string.find(start, sortBy)
-            local pref = string.sub(start, 1, prefEnd)
-            if first == pref then
-                introduction = introduction .. start .. ", "
+        local currentItem = tostring( array[i] )
+        if (insertLineBreak) then
+            local prefixEnd = string.find( currentItem, prefixSplit )
+            local currentPrefix = string.sub( currentItem, 1, prefixEnd )
+            if previousPrefix == currentPrefix then
+                output = output .. currentItem .. ", "
             else
-                first = pref
-                introduction = introduction .. "\n" .. start .. ", "
+                previousPrefix = currentPrefix
+                output = output .. "\n" .. currentItem .. ", "
             end
         else
-            introduction = introduction .. start .. ", "
+            output = output .. currentItem .. ", "
         end
     end
-    return introduction
+
+    return output
 end
 
 return helper
 
+-- GENERAL USEFUL COMMANDS
+--
+-- Get all player lines
+--   api.engine.system.lineSystem.getLinesForPlayer( api.engine.util.getPlayer() )
+--
+-- Get vehicles of a specific line (make sure to replace "line_id")
+--   api.engine.system.transportVehicleSystem.getLineVehicles( line_id )
+--
+-- Get transport vehicle info (make sure to replace "vehicle_id")
+--   api.engine.getComponent(vehicle_id, api.type.ComponentType.TRANSPORT_VEHICLE)
+--
+-- Or combine into one call (make sure to replace "line_id")
+--   api.engine.getComponent( api.engine.system.transportVehicleSystem.getLineVehicles( line_id )[1], api.type.ComponentType.TRANSPORT_VEHICLE )
+--
 -- api.engine.getComponent(line_id, api.type.ComponentType.LINE)
 -- vehicleInfo = {
 --    transportModes = {
---      [1] = 0,
---      [2] = 0,
---      [3] = 0,
---      [4] = 0, ROAD
---      [5] = 0,
---      [6] = 0, TRAM
---      [7] = 0, ELECTRIC_TRAM
---      [8] = 0, RAIL STEAM
---      [9] = 0, RAIL
---      [10] = 0, AIR
---      [11] = 0, WATER LARGE
---      [12] = 0,
---      [13] = 0, WATER SMALL
+--      [1]  = 0,
+--      [2]  = 0,
+--      [3]  = 0,
+--      [4]  = 0, ROAD
+--      [5]  = 0,
+--      [6]  = 0,
+--      [7]  = 0, TRAM
+--      [8]  = 0, ELECTRIC_TRAM
+--      [9]  = 0, RAIL
+--      [10] = 0, AIR?
+--      [11] = 0, WATER_LARGE
+--      [12] = 0, AIR
+--      [13] = 0, WATER_SMALL
 --      [14] = 0,
 --      [15] = 0,
 --      [16] = 0,
@@ -330,15 +353,15 @@ return helper
 -- api.engine.getComponent(vehicle_id, api.type.ComponentType.TRANSPORT_VEHICLE)
 -- config = {
 --    capacities = {
---      [1] = 0, PASSENGERS
---      [2] = 0,
---      [3] = 0,
---      [4] = 0,
---      [5] = 0, STONE
---      [6] = 0,
---      [7] = 0, CRUDE OIL
---      [8] = 0,
---      [9] = 0,
+--      [1]  = 0, PASSENGERS
+--      [2]  = 0,
+--      [3]  = 0,
+--      [4]  = 0,
+--      [5]  = 0, STONE
+--      [6]  = 0,
+--      [7]  = 0, CRUDE_OIL
+--      [8]  = 0,
+--      [9]  = 0,
 --      [10] = 0,
 --      [11] = 0, OIL
 --      [12] = 0,
