bb92fc642e831843250d6e78095232b3ec1e9809
Tommy Carlsson <tommyc81@gmail.com>
2023-02-18 11:18:32 +0400

    Update version number.

--- mod.lua
+++ mod.lua
@@ -3,7 +3,7 @@ function data()
         info = {
             name = _("Name"),
             description = _("Description"),
-            minorVersion = 34,
+            minorVersion = 35,
             severityAdd = "NONE",
             severityRemove = "NONE",
             tags = { "Script Mod" },
